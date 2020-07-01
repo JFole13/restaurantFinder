58aa53fba76af71d47bd3962a353802aa8b937e9
Jesse Foley <jessefoley10@yahoo.com>
2020-07-01 01:12:43 -0500

    Added basic layout and styled the left container

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // First Commit
+// Split Screen Design
+// Will use white and a light red as a theme
+// left side is where city selector will be
+// right side is the results
+// modals maybe
+//Edge cases: states with duplicate city names, 404 city not found protocol 
