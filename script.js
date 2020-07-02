2e265e6adacae9b08696dbcd96b9e2e310fb5420
Jesse Foley <jessefoley10@yahoo.com>
2020-07-02 12:23:51 -0500

    Added styling to the right side, minus the imgs of the example cards

--- script.js
+++ script.js
@@ -4,4 +4,4 @@
 // left side is where city selector will be
 // right side is the results
 // modals maybe
-//Edge cases: states with duplicate city names, 404 city not found protocol 
+//Edge cases: states with duplicate city names, 404 city not found protocol jhjhgjghjhgjghjhg
